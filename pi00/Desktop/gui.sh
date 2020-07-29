0c5e8feb4674c150118436caf88d33a67678e9fe
andysign <andysignerdesigner@gmail.com>
2020-07-29 23:17:12 +0100

    Better gui sh

--- pi00/Desktop/gui.sh
+++ pi00/Desktop/gui.sh
@@ -1,2 +1,2 @@
 pinum=$(hostname|tr "raspberrypi" "\n"|tail -1)
-/bin/bash -c "source /home/pi/pi00/bin/activate; python /home/pi/pi00/gui.py"
+/bin/bash -c "cd /home/pi/${pinum}; source bin/activate; python gui.py"
