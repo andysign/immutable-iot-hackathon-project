70cc2ca60965b95364dd19f51bc84d730ca9b859
andysign <andysignerdesigner@gmail.com>
2020-07-29 15:25:59 +0100

    Smallfix

--- pi02/start-virtualenv.sh
+++ pi02/start-virtualenv.sh
@@ -1,4 +1,4 @@
 num=$(hostname|tr "raspberrypi" "\n"|tail -1)
 cd ~/pi${num}
 [ -d "bin/" ] && echo "" || virtualenv --python=/usr/bin/python3.7 ~/pi${num}
-source ~/pi${num}/bin/activate
+source /home/pi/pi${num}/bin/activate
