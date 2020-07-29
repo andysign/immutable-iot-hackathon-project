30f7b12355f38c1e47e387955be31905e2b90b9d
andysign <andysignerdesigner@gmail.com>
2020-07-29 23:22:49 +0100

    Better start env

--- pi02/start-virtualenv.sh
+++ pi02/start-virtualenv.sh
@@ -1,6 +1,5 @@
 # sudo apt install virtualenv
-echo ". ./start-virtualenv.sh"
 num=$(hostname|tr "raspberrypi" "\n"|tail -1)
 cd ~/pi${num}
 [ -d "bin/" ] && echo "" || virtualenv --python=/usr/bin/python3.7 ~/pi${num}
-source /home/pi/pi${num}/bin/activate
+/bin/bash -c "source /home/pi/pi${num}/bin/activate ; exec /bin/bash -i"
