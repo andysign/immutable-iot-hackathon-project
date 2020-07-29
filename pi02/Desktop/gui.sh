638ffd031fa9391702032472766f6c9775d68012
andysign <andysignerdesigner@gmail.com>
2020-07-29 23:25:47 +0100

    Fix guish

--- pi02/Desktop/gui.sh
+++ pi02/Desktop/gui.sh
@@ -1,3 +1,3 @@
 # ln -s /home/pi/pi$(hostname|tr "raspberrypi" "\n"|tail -1)/Desktop/ /home/pi/
 pinum=$(hostname|tr "raspberrypi" "\n"|tail -1)
-/bin/bash -c "cd /home/pi/${pinum}; source bin/activate; python gui.py"
+/bin/bash -c "cd /home/pi/pi${pinum}; source bin/activate; python gui.py"
